2d1a188d6b0b73896455a1fc850744743388caca
cybergravity <pankaj6711@gmail.com>
2020-04-28 13:53:16 +0530

    commit

--- telusko/settings.py
+++ telusko/settings.py
@@ -141,19 +141,16 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-    ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = 'telusko/static/'
+
+
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage' 
 
 
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'telusko/static'),
-)
+
 
 
 
