e76809cf45622e91ed9b3d03360f05ce3edc6ebf
cybergravity <pankaj6711@gmail.com>
2020-04-28 14:01:25 +0530

    commit

--- telusko/settings.py
+++ telusko/settings.py
@@ -137,9 +137,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
