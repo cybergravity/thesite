8b332b1e03d081301c67bae2e1655c1d8d7249de
cybergravity <pankaj6711@gmail.com>
2020-04-29 19:27:12 +0530

    commit

--- telusko/settings.py
+++ telusko/settings.py
@@ -82,26 +82,13 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'know',
-#         'USER': 'postgres',
-#         'PASSWORD': '0258',
-#         'HOST': 'localhost'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tmk',
-        'USER': 'postgres',
-        'PASSWORD': '0258',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
