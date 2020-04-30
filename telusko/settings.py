841e19f6fd4cda2deb5b9186056676696fc5a498
cybergravity <pankaj6711@gmail.com>
2020-04-30 17:40:17 +0530

    c

--- telusko/settings.py
+++ telusko/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'telusko.wsgi.application'
 
 
 # Database
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -92,14 +92,12 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-'''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
+
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
